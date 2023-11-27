4a48f558084ecdfd7bca78b9ce531aa9ccf6fbb5
Marcelo 3K <marcelo3k.dev@gmail.com>
2023-11-27 19:31:42 -0300

    Update dependecies

---
+++ src/app.jsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <>
+      <h1>Boilerplate</h1>
+    </>
+  )
+}
+
+export { App }
---
+++ src/mock/movies.ts
@@ -0,0 +1,13 @@
+const movies = [
+  {
+    thumbnail: '',
+    name: 'Intocaveis',
+    genre: 'Drama, Comédia, Biopic',
+    duration: 5400,
+    rating: 9.7,
+    sinopse: 'Descrição',
+    cast: '',
+    directedBy: '',
+    publishedAt: new Date()
+  }
+]
