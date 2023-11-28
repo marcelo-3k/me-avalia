d7af0b57d9f155a08b656043820acb2b7df86491
Marcelo 3K <marcelo3k.dev@gmail.com>
2023-11-28 20:11:25 -0300

    Fix NaN error at getToalMinutes function

--- src/app.jsx
+++ src/app.jsx
@@ -36,8 +36,32 @@ const App = () => {
 
   const handleClickDelete = (id) => setWatchedMovies((prev) => prev.filter(movie => movie.id !== id))
   const handleSelectBack = () => setSelectedMovie(null)
-  const handleSelectMovie = (selectedMovie) => 
-    setSelectedMovie(prev => prev?.id === selectedMovie.id ? null : selectedMovie)
+  const handleSelectMovie = async (currentSelectedMovie) => {
+    const prevSelectedMovie = selectedMovie
+
+    if (prevSelectedMovie?.id === currentSelectedMovie.id) {
+      setSelectedMovie(null)
+      return
+    }
+
+    const url = `https://www.omdbapi.com/?apikey=${apiKey}&i=${currentSelectedMovie.id}`
+    const response = await fetch(url)
+    const movie = await response.json()
+
+    setSelectedMovie({
+      id: movie.imdbID,
+      title: movie.Title,
+      year: movie.Year,
+      poster: movie.Poster,
+      imdbRating: movie.imdbRating,
+      runtime: movie.Runtime,
+      plot: movie.Plot,
+      actors: movie.Actors,
+      director: movie.Director,
+      released: movie.Released,
+      genre: movie.Genre
+    })
+  }
 
   const handleSearchMovie = async (e) => {
     e.preventDefault()
--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,6 +1,6 @@
 export const getTotalMinutes = (watchedMovies) => {
   const totalMinutes = watchedMovies
-    .reduce((acc, movie) => acc + +movie.runtime.split(' ')[0], 0);
+    .reduce((acc, movie) => acc + (movie.runtime === 'N/A' ? 0 : +movie.runtime.split(' ')[0]), 0);
 
   const hours = Math.floor(totalMinutes / 60);
   const minutes = totalMinutes % 60;
